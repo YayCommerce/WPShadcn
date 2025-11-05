aff9bef9f47139cf0c7a746fa3a4700505b25b60
tracedv96 <thien.nhatrangart@gmail.com>
2025-11-05 19:45:24 +0700

    Update checkout header

---
+++ patterns/header-2.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: Header 2
+ * Slug: shadcn/header-2
+ * Categories: shadcn, header
+ * Description: A header with logo, title, tagline, dark mode toggle, navigation, and customer account and mini cart.
+ */
+
+$darkmode_image_url = get_template_directory_uri() . '/assets/images/dark-mode.png';
+
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|4","padding":{"top":"var:preset|spacing|4","bottom":"var:preset|spacing|4","left":"0","right":"0"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+	<div class="wp-block-group alignwide"
+		style="padding-top:var(--wp--preset--spacing--4);padding-right:0;padding-bottom:var(--wp--preset--spacing--4);padding-left:0">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|4"}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"center"}} -->
+		<div class="wp-block-group">
+			<!-- wp:site-logo {"width":40,"shouldSyncIcon":true,"style":{"border":{"radius":"0.375rem"}}} /-->
+
+			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|1","padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+				<!-- wp:site-title {"level":0,"style":{"typography":{"fontWeight":"600","textDecoration":"none","fontStyle":"normal"}},"fontSize":"2-xl"} /-->
+
+				<!-- wp:site-tagline {"style":{"typography":{"fontSize":"var:preset|font-size|sm"},"color":{"text":"var:preset|color|muted-foreground"}}} /-->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+		<div class="wp-block-group">
+			<!-- wp:image {"id":12,"width":"20px","sizeSlug":"full","linkDestination":"none","className":"dark-mode-toggle","style":{"color":{"duotone":["rgb(0, 0, 0)","rgb(255, 255, 255)"]}}} -->
+			<figure class="wp-block-image size-full is-resized dark-mode-toggle"><img
+					src="<?php echo esc_url( $darkmode_image_url ); ?>" alt="" class="wp-image-12"
+					style="width:20px" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
