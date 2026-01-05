be49dfe1e4c2bccc5fc911762f3a29170200c7e3
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2026-01-05 17:32:55 +0700

    Add pattern Incentives 5

---
+++ patterns/incentives-5.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Title: Incentives 5
+ * Slug: shadcn/incentives-5
+ * Categories: shadcn, incentives
+ * Description: A grid of incentives showcasing key benefits or services.
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"categories":["shadcn"],"patternName":"shadcn/incentives-5","name":"Incentives 5"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|4","right":"var:preset|spacing|4"}},"color":{"background":"var:preset|color|background","text":"var:preset|color|foreground"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull has-text-color has-background" style="color:var(--wp--preset--color--foreground);background-color:var(--wp--preset--color--background);padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--4);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--4)"><!-- wp:group {"metadata":{"name":"Content"},"style":{"spacing":{"blockGap":"16px"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"22rem"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Icon"},"className":"is-style-default","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px","bottomLeft":"8px","bottomRight":"8px"},"color":"#e5e5e5","width":"1px"},"spacing":{"padding":{"top":"10px","bottom":"10px","left":"10px","right":"10px"},"margin":{"bottom":"var:preset|spacing|4"}},"layout":{"selfStretch":"fixed","flexSize":"40px"},"dimensions":{"minHeight":""},"shadow":" 0px 1px 2px 0px #0000000d"},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group is-style-default has-border-color has-background-background-color has-background" style="border-color:#e5e5e5;border-width:1px;border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-left-radius:8px;border-bottom-right-radius:8px;margin-bottom:var(--wp--preset--spacing--4);padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;box-shadow: 0px 1px 2px 0px #0000000d"><!-- wp:image {"svgCode":"\u003csvg width=\u002219\u0022 height=\u002216\u0022 viewBox=\u00220 0 19 16\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M11 12.6667V2.66667C11 2.22464 10.8244 1.80072 10.5118 1.48816C10.1993 1.17559 9.77536 1 9.33333 1H2.66667C2.22464 1 1.80072 1.17559 1.48816 1.48816C1.17559 1.80072 1 2.22464 1 2.66667V11.8333C1 12.0543 1.0878 12.2663 1.24408 12.4226C1.40036 12.5789 1.61232 12.6667 1.83333 12.6667H3.5M3.5 12.6667C3.5 13.5871 4.24619 14.3333 5.16667 14.3333C6.08714 14.3333 6.83333 13.5871 6.83333 12.6667M3.5 12.6667C3.5 11.7462 4.24619 11 5.16667 11C6.08714 11 6.83333 11.7462 6.83333 12.6667M11.8333 12.6667H6.83333M11.8333 12.6667C11.8333 13.5871 12.5795 14.3333 13.5 14.3333C14.4205 14.3333 15.1667 13.5871 15.1667 12.6667M11.8333 12.6667C11.8333 11.7462 12.5795 11 13.5 11C14.4205 11 15.1667 11.7462 15.1667 12.6667M15.1667 12.6667H16.8333C17.0543 12.6667 17.2663 12.5789 17.4226 12.4226C17.5789 12.2663 17.6667 12.0543 17.6667 11.8333V8.79167C17.6663 8.60255 17.6017 8.41918 17.4833 8.27167L14.5833 4.64667C14.5054 4.54907 14.4065 4.47023 14.294 4.416C14.1815 4.36176 14.0582 4.33351 13.9333 4.33333H11\u0022 stroke=\u0022#0A0A0A\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e\n","svgColor":"#0a0a0a","svgSize":"20px","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|3"}},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"foreground"} -->
+<h4 class="wp-block-heading has-foreground-color has-text-color has-link-color" style="margin-bottom:var(--wp--preset--spacing--3);font-style:normal;font-weight:500">Free 2-day shipping</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"var:preset|font-size|sm","lineHeight":"1.6"}},"textColor":"muted-foreground"} -->
+<p class="has-text-align-left has-muted-foreground-color has-text-color" style="font-size:var(--wp--preset--font-size--sm);line-height:1.6">Complimentary express shipping on every order over $75, anywhere in the continental US.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Icon"},"className":"is-style-default","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px","bottomLeft":"8px","bottomRight":"8px"},"color":"#e5e5e5","width":"1px"},"spacing":{"padding":{"top":"10px","bottom":"10px","left":"10px","right":"10px"},"margin":{"bottom":"var:preset|spacing|4"}},"layout":{"selfStretch":"fixed","flexSize":"40px"},"dimensions":{"minHeight":""},"shadow":" 0px 1px 2px 0px #0000000d"},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group is-style-default has-border-color has-background-background-color has-background" style="border-color:#e5e5e5;border-width:1px;border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-left-radius:8px;border-bottom-right-radius:8px;margin-bottom:var(--wp--preset--spacing--4);padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;box-shadow: 0px 1px 2px 0px #0000000d"><!-- wp:image {"svgCode":"\u003csvg width=\u002217\u0022 height=\u002217\u0022 viewBox=\u00220 0 17 17\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M16 8.5C16 6.51088 15.2098 4.60322 13.8033 3.1967C12.3968 1.79018 10.4891 1 8.5 1C6.40329 1.00789 4.39081 1.82602 2.88333 3.28333L1 5.16667M1 5.16667V1M1 5.16667H5.16667M1 8.5C1 10.4891 1.79018 12.3968 3.1967 13.8033C4.60322 15.2098 6.51088 16 8.5 16C10.5967 15.9921 12.6092 15.174 14.1167 13.7167L16 11.8333M16 11.8333H11.8333M16 11.8333V16\u0022 stroke=\u0022#0A0A0A\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e\n","svgColor":"#0a0a0a","svgSize":"20px","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|3"}},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"foreground"} -->
+<h4 class="wp-block-heading has-foreground-color has-text-color has-link-color" style="margin-bottom:var(--wp--preset--spacing--3);font-style:normal;font-weight:500">30-day free returns</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"var:preset|font-size|sm","lineHeight":"1.6"}},"textColor":"muted-foreground"} -->
+<p class="has-text-align-left has-muted-foreground-color has-text-color" style="font-size:var(--wp--preset--font-size--sm);line-height:1.6">Changed your mind? Send items back within 30 days for a fast refund with no restocking fees.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Icon"},"className":"is-style-default","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px","bottomLeft":"8px","bottomRight":"8px"},"color":"#e5e5e5","width":"1px"},"spacing":{"padding":{"top":"10px","bottom":"10px","left":"10px","right":"10px"},"margin":{"bottom":"var:preset|spacing|4"}},"layout":{"selfStretch":"fixed","flexSize":"40px"},"dimensions":{"minHeight":""},"shadow":" 0px 1px 2px 0px #0000000d"},"backgroundColor":"background","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","verticalAlignment":"center"}} -->
+<div class="wp-block-group is-style-default has-border-color has-background-background-color has-background" style="border-color:#e5e5e5;border-width:1px;border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-left-radius:8px;border-bottom-right-radius:8px;margin-bottom:var(--wp--preset--spacing--4);padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;box-shadow: 0px 1px 2px 0px #0000000d"><!-- wp:image {"svgCode":"\u003csvg width=\u002216\u0022 height=\u002219\u0022 viewBox=\u00220 0 16 19\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M5.16667 9.33374L6.83333 11.0004L10.1667 7.66707M14.3333 10.167C14.3333 14.3337 11.4167 16.417 7.95 17.6254C7.76847 17.6869 7.57128 17.6839 7.39167 17.617C3.91667 16.417 1 14.3337 1 10.167V4.33371C1 4.11269 1.0878 3.90073 1.24408 3.74445C1.40036 3.58817 1.61232 3.50037 1.83333 3.50037C3.5 3.50037 5.58333 2.50037 7.03333 1.23371C7.20988 1.08287 7.43446 1 7.66667 1C7.89887 1 8.12345 1.08287 8.3 1.23371C9.75833 2.50871 11.8333 3.50037 13.5 3.50037C13.721 3.50037 13.933 3.58817 14.0893 3.74445C14.2455 3.90073 14.3333 4.11269 14.3333 4.33371V10.167Z\u0022 stroke=\u0022#0A0A0A\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e","svgColor":"#0a0a0a","svgSize":"20px","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|3"}},"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"foreground"} -->
+<h4 class="wp-block-heading has-foreground-color has-text-color has-link-color" style="margin-bottom:var(--wp--preset--spacing--3);font-style:normal;font-weight:500">Secure checkout</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"var:preset|font-size|sm","lineHeight":"1.6"}},"textColor":"muted-foreground"} -->
+<p class="has-text-align-left has-muted-foreground-color has-text-color" style="font-size:var(--wp--preset--font-size--sm);line-height:1.6">256-bit SSL encryption plus Buyer Protection on every purchase keeps your payment info safe.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
