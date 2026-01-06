c5dd9c5749990597d6e36fc28570a4ac274ca8a8
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2026-01-06 11:37:35 +0700

    Add pattern flash sale

---
+++ patterns/flash-sale.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Flash Sale
+ * Slug: shadcn/flash-sale
+ * Categories: shadcn, flash-sale
+ * Description: A flash sale section with title, description, and call-to-action buttons.
+ */
+
+?>
+
+<!-- wp:group {"tagName":"main","metadata":{"categories":["shadcn"],"patternName":"shadcn/flash-sale","name":"Flash Sale"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|6","bottom":"var:preset|spacing|6","left":"var:preset|spacing|4","right":"var:preset|spacing|4"}},"border":{"radius":{"topLeft":"10px","topRight":"10px","bottomLeft":"10px","bottomRight":"10px"}}},"layout":{"type":"default"}} -->
+<main class="wp-block-group alignfull" style="border-top-left-radius:10px;border-top-right-radius:10px;border-bottom-left-radius:10px;border-bottom-right-radius:10px;padding-top:var(--wp--preset--spacing--6);padding-right:var(--wp--preset--spacing--4);padding-bottom:var(--wp--preset--spacing--6);padding-left:var(--wp--preset--spacing--4)"><!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<h1 class="wp-block-heading has-text-align-left" style="font-style:normal;font-weight:600">Flash Sale! 33% off!</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"var:preset|font-size|lg","lineHeight":"1.6"},"elements":{"link":{"color":{"text":"var:preset|color|muted-foreground"}}}},"textColor":"muted-foreground"} -->
+<p class="has-text-align-left has-muted-foreground-color has-text-color has-link-color" style="font-size:var(--wp--preset--font-size--lg);line-height:1.6">Hurry up! The offer ends soon.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"style":{"spacing":{"blockGap":"var:preset|spacing|4"}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill","style":{"border":{"radius":{"topLeft":"8px","topRight":"8px","bottomLeft":"8px","bottomRight":"8px"}},"typography":{"fontStyle":"normal","fontWeight":"500"},"spacing":{"padding":{"left":"var:preset|spacing|6","right":"var:preset|spacing|6"}}}} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" style="border-top-left-radius:8px;border-top-right-radius:8px;border-bottom-left-radius:8px;border-bottom-right-radius:8px;padding-right:var(--wp--preset--spacing--6);padding-left:var(--wp--preset--spacing--6);font-style:normal;font-weight:500">Shop now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></main>
+<!-- /wp:group -->
