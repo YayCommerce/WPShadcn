f31bbedaba2143f66d12e7539965e69e614de0e3
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2026-01-06 16:07:04 +0700

    Paul/feat/patterns incentives (#3)
    * Add pattern incentives 1
    
    * Update pattern
    
    * Add pattern incentives 4
    
    * Add pattern Incentives 5
    
    * Update incentives-4.php
    
    * Add pattern Incentives 6
    
    * Update pattern incentives 1
    
    * Update svg colors & layout
    
    ---------
    
    Co-authored-by: Trace <75064066+thithiennta@users.noreply.github.com>

---
+++ patterns/incentives-6.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Title: Incentives 6
+ * Slug: shadcn/incentives-6
+ * Categories: shadcn, incentives
+ * Description: A grid of incentives showcasing key benefits or services.
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"categories":["shadcn","incentives"],"patternName":"shadcn/incentives-6","name":"Incentives 6"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|4","right":"var:preset|spacing|4"}},"color":{"background":"var:preset|color|background","text":"var:preset|color|foreground"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide has-text-color has-background" style="color:var(--wp--preset--color--foreground);background-color:var(--wp--preset--color--background);padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--4);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--4)"><!-- wp:group {"metadata":{"name":"Content"},"style":{"spacing":{"blockGap":"var:preset|spacing|6"}},"layout":{"type":"grid","columnCount":null,"minimumColumnWidth":"16rem"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"dimensions":{"minHeight":""},"spacing":{"blockGap":"var:preset|spacing|4"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"svgCode":"\u003csvg width=\u002222\u0022 height=\u002218\u0022 viewBox=\u00220 0 22 18\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M13 15V3C13 2.46957 12.7893 1.96086 12.4142 1.58579C12.0391 1.21071 11.5304 1 11 1H3C2.46957 1 1.96086 1.21071 1.58579 1.58579C1.21071 1.96086 1 2.46957 1 3V14C1 14.2652 1.10536 14.5196 1.29289 14.7071C1.48043 14.8946 1.73478 15 2 15H4M4 15C4 16.1046 4.89543 17 6 17C7.10457 17 8 16.1046 8 15M4 15C4 13.8954 4.89543 13 6 13C7.10457 13 8 13.8954 8 15M14 15H8M14 15C14 16.1046 14.8954 17 16 17C17.1046 17 18 16.1046 18 15M14 15C14 13.8954 14.8954 13 16 13C17.1046 13 18 13.8954 18 15M18 15H20C20.2652 15 20.5196 14.8946 20.7071 14.7071C20.8946 14.5196 21 14.2652 21 14V10.35C20.9996 10.1231 20.922 9.90301 20.78 9.726L17.3 5.376C17.2065 5.25888 17.0878 5.16428 16.9528 5.0992C16.8178 5.03412 16.6699 5.00021 16.52 5H13\u0022 stroke=\u0022currentColor\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e\n","svgColor":"currentColor","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"xl"} -->
+<p class="has-xl-font-size">Free 2-day shipping</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":""},"spacing":{"blockGap":"var:preset|spacing|3"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"svgCode":"\u003csvg width=\u002220\u0022 height=\u002220\u0022 viewBox=\u00220 0 20 20\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M19 10C19 7.61305 18.0518 5.32387 16.364 3.63604C14.6761 1.94821 12.3869 1 10 1C7.48395 1.00947 5.06897 1.99122 3.26 3.74L1 6M1 6V1M1 6H6M1 10C1 12.3869 1.94821 14.6761 3.63604 16.364C5.32387 18.0518 7.61305 19 10 19C12.516 18.9905 14.931 18.0088 16.74 16.26L19 14M19 14H14M19 14V19\u0022 stroke=\u0022currentColor\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e\n","svgColor":"currentColor","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"xl"} -->
+<p class="has-xl-font-size">30-day free returns</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":""},"spacing":{"blockGap":"var:preset|spacing|3"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"svgCode":"\u003csvg width=\u002218\u0022 height=\u002222\u0022 viewBox=\u00220 0 18 22\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M6 11.0005L8 13.0005L12 9.00049M17 12.0004C17 17.0004 13.5 19.5005 9.34 20.9505C9.12216 21.0243 8.88554 21.0207 8.67 20.9405C4.5 19.5005 1 17.0004 1 12.0004V5.00045C1 4.73523 1.10536 4.48088 1.29289 4.29334C1.48043 4.10581 1.73478 4.00045 2 4.00045C4 4.00045 6.5 2.80045 8.24 1.28045C8.45185 1.09945 8.72135 1 9 1C9.27865 1 9.54815 1.09945 9.76 1.28045C11.51 2.81045 14 4.00045 16 4.00045C16.2652 4.00045 16.5196 4.10581 16.7071 4.29334C16.8946 4.48088 17 4.73523 17 5.00045V12.0004Z\u0022 stroke=\u0022currentColor\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e\n","svgColor":"currentColor","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"xl"} -->
+<p class="has-xl-font-size">Secure checkout</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"dimensions":{"minHeight":""},"spacing":{"blockGap":"var:preset|spacing|3"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"svgCode":"\u003csvg width=\u002220\u0022 height=\u002222\u0022 viewBox=\u00220 0 20 22\u0022 fill=\u0022none\u0022 xmlns=\u0022http://www.w3.org/2000/svg\u0022\u003e\n\u003cpath d=\u0022M1 10H4C4.53043 10 5.03914 10.2107 5.41421 10.5858C5.78929 10.9609 6 11.4696 6 12V15C6 15.5304 5.78929 16.0391 5.41421 16.4142C5.03914 16.7893 4.53043 17 4 17H3C2.46957 17 1.96086 16.7893 1.58579 16.4142C1.21071 16.0391 1 15.5304 1 15V10ZM1 10C1 8.8181 1.23279 7.64778 1.68508 6.55585C2.13738 5.46392 2.80031 4.47177 3.63604 3.63604C4.47177 2.80031 5.46392 2.13738 6.55585 1.68508C7.64778 1.23279 8.8181 1 10 1C11.1819 1 12.3522 1.23279 13.4442 1.68508C14.5361 2.13738 15.5282 2.80031 16.364 3.63604C17.1997 4.47177 17.8626 5.46392 18.3149 6.55585C18.7672 7.64778 19 8.8181 19 10M19 10V15M19 10H16C15.4696 10 14.9609 10.2107 14.5858 10.5858C14.2107 10.9609 14 11.4696 14 12V15C14 15.5304 14.2107 16.0391 14.5858 16.4142C14.9609 16.7893 15.4696 17 16 17H17C17.5304 17 18.0391 16.7893 18.4142 16.4142C18.7893 16.0391 19 15.5304 19 15M19 15V17C19 18.0609 18.5786 19.0783 17.8284 19.8284C17.0783 20.5786 16.0609 21 15 21H10\u0022 stroke=\u0022currentColor\u0022 stroke-width=\u00222\u0022 stroke-linecap=\u0022round\u0022 stroke-linejoin=\u0022round\u0022/\u003e\n\u003c/svg\u003e\n","svgColor":"currentColor","className":"is-style-rounded"} -->
+<figure class="wp-block-image is-style-rounded"><img alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"xl"} -->
+<p class="has-xl-font-size">24/7 support</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
