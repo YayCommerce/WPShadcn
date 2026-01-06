a38d4583170948b410b521c997636c7e0a32ed25
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2026-01-06 14:36:55 +0700

    Add pattern best deals

---
+++ patterns/best-deals-1.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Title: Best Deals 1
+ * Slug: shadcn/best-deals-1
+ * Categories: shadcn, best-deals
+ * Description: Grid of best deals product cards section.
+ */
+
+?>
+
+<!-- wp:group {"tagName":"main","metadata":{"categories":["shadcn"],"patternName":"shadcn/best-deals-1","name":"Best Deals 1"},"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|6","bottom":"var:preset|spacing|6","left":"var:preset|spacing|4","right":"var:preset|spacing|4"},"blockGap":"var:preset|spacing|6"}},"layout":{"type":"default"}} -->
+<main class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--6);padding-right:var(--wp--preset--spacing--4);padding-bottom:var(--wp--preset--spacing--6);padding-left:var(--wp--preset--spacing--4)"><!-- wp:heading {"textAlign":"left","level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}}} -->
+<h1 class="wp-block-heading has-text-align-left" style="font-style:normal;font-weight:600">Best deals ending soon</h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"var:preset|spacing|4"},"dimensions":{"minHeight":""}},"layout":{"type":"grid","minimumColumnWidth":"16rem"}} -->
+<div class="wp-block-group is-style-default" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"background":{"backgroundImage":{"url":"https://shadcn.test/wp-content/uploads/2026/01/Aspect-Ratio-1.png","id":63,"source":"file","title":"Aspect Ratio"},"backgroundSize":"contain","backgroundRepeat":"no-repeat"},"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"200px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|4","margin":{"top":"20px","bottom":"20px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:20px;margin-bottom:20px"><!-- wp:paragraph {"align":"center","className":"is-style-badge","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"color":{"background":"#000000","text":"#ffffff"},"border":{"radius":{"topLeft":"9999px","topRight":"9999px","bottomLeft":"9999px","bottomRight":"9999px"}},"typography":{"fontSize":"12px"}},"textColor":"background"} -->
+<p class="has-text-align-center is-style-badge has-background-color has-text-color has-background has-link-color" style="border-top-left-radius:9999px;border-top-right-radius:9999px;border-bottom-left-radius:9999px;border-bottom-right-radius:9999px;color:#ffffff;background-color:#000000;font-size:12px">33% off</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"color":{"text":"#ffffff"}},"textColor":"background","fontSize":"2-xl"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-2-xl-font-size" style="color:#ffffff;font-style:normal;font-weight:600">Men's Apparel</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"foreground","style":{"color":{"background":"#ffffff"},"border":{"radius":"8px","color":"#e5e5e5","width":"1px","style":"solid"},"spacing":{"padding":{"left":"var:preset|spacing|5","right":"var:preset|spacing|5"}},"typography":{"fontWeight":"400","fontStyle":"normal","fontSize":"14px"},"shadow":" 0px 1px 2px 0px #0000000d"}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-foreground-color has-text-color has-background has-border-color has-custom-font-size wp-element-button" style="border-color:#e5e5e5;border-style:solid;border-width:1px;border-radius:8px;background-color:#ffffff;padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5);box-shadow: 0px 1px 2px 0px #0000000d;font-size:14px;font-style:normal;font-weight:400">Shop now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"https://shadcn.test/wp-content/uploads/2026/01/Aspect-Ratio-1.png","id":63,"source":"file","title":"Aspect Ratio"},"backgroundSize":"contain","backgroundRepeat":"no-repeat"},"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"200px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|4","margin":{"top":"20px","bottom":"20px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:20px;margin-bottom:20px"><!-- wp:paragraph {"align":"center","className":"is-style-badge","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"color":{"background":"#000000","text":"#ffffff"},"border":{"radius":{"topLeft":"9999px","topRight":"9999px","bottomLeft":"9999px","bottomRight":"9999px"}},"typography":{"fontSize":"12px"}},"textColor":"background"} -->
+<p class="has-text-align-center is-style-badge has-background-color has-text-color has-background has-link-color" style="border-top-left-radius:9999px;border-top-right-radius:9999px;border-bottom-left-radius:9999px;border-bottom-right-radius:9999px;color:#ffffff;background-color:#000000;font-size:12px">50% off</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"color":{"text":"#ffffff"}},"textColor":"background","fontSize":"2-xl"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-2-xl-font-size" style="color:#ffffff;font-style:normal;font-weight:600">Women's Fashion</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"foreground","style":{"color":{"background":"#ffffff"},"border":{"radius":"8px","color":"#e5e5e5","width":"1px","style":"solid"},"spacing":{"padding":{"left":"var:preset|spacing|5","right":"var:preset|spacing|5"}},"typography":{"fontWeight":"400","fontStyle":"normal","fontSize":"14px"},"shadow":" 0px 1px 2px 0px #0000000d"}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-foreground-color has-text-color has-background has-border-color has-custom-font-size wp-element-button" style="border-color:#e5e5e5;border-style:solid;border-width:1px;border-radius:8px;background-color:#ffffff;padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5);box-shadow: 0px 1px 2px 0px #0000000d;font-size:14px;font-style:normal;font-weight:400">Shop now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"https://shadcn.test/wp-content/uploads/2026/01/Aspect-Ratio-1.png","id":63,"source":"file","title":"Aspect Ratio"},"backgroundSize":"contain","backgroundRepeat":"no-repeat"},"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"200px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|4","margin":{"top":"20px","bottom":"20px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:20px;margin-bottom:20px"><!-- wp:paragraph {"align":"center","className":"is-style-badge","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"color":{"background":"#000000","text":"#ffffff"},"border":{"radius":{"topLeft":"9999px","topRight":"9999px","bottomLeft":"9999px","bottomRight":"9999px"}},"typography":{"fontSize":"12px"}},"textColor":"background"} -->
+<p class="has-text-align-center is-style-badge has-background-color has-text-color has-background has-link-color" style="border-top-left-radius:9999px;border-top-right-radius:9999px;border-bottom-left-radius:9999px;border-bottom-right-radius:9999px;color:#ffffff;background-color:#000000;font-size:12px">20% off</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"color":{"text":"#ffffff"}},"textColor":"background","fontSize":"2-xl"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-2-xl-font-size" style="color:#ffffff;font-style:normal;font-weight:600">Accessories</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"foreground","style":{"color":{"background":"#ffffff"},"border":{"radius":"8px","color":"#e5e5e5","width":"1px","style":"solid"},"spacing":{"padding":{"left":"var:preset|spacing|5","right":"var:preset|spacing|5"}},"typography":{"fontWeight":"400","fontStyle":"normal","fontSize":"14px"},"shadow":" 0px 1px 2px 0px #0000000d"}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-foreground-color has-text-color has-background has-border-color has-custom-font-size wp-element-button" style="border-color:#e5e5e5;border-style:solid;border-width:1px;border-radius:8px;background-color:#ffffff;padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5);box-shadow: 0px 1px 2px 0px #0000000d;font-size:14px;font-style:normal;font-weight:400">Shop now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"background":{"backgroundImage":{"url":"https://shadcn.test/wp-content/uploads/2026/01/Aspect-Ratio-1.png","id":63,"source":"file","title":"Aspect Ratio"},"backgroundSize":"contain","backgroundRepeat":"no-repeat"},"layout":{"columnSpan":1,"rowSpan":1}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer {"height":"200px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:200px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|4","margin":{"top":"20px","bottom":"20px"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"bottom","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:20px;margin-bottom:20px"><!-- wp:paragraph {"align":"center","className":"is-style-badge","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"color":{"background":"#000000","text":"#ffffff"},"border":{"radius":{"topLeft":"9999px","topRight":"9999px","bottomLeft":"9999px","bottomRight":"9999px"}},"typography":{"fontSize":"12px"}},"textColor":"background"} -->
+<p class="has-text-align-center is-style-badge has-background-color has-text-color has-background has-link-color" style="border-top-left-radius:9999px;border-top-right-radius:9999px;border-bottom-left-radius:9999px;border-bottom-right-radius:9999px;color:#ffffff;background-color:#000000;font-size:12px">10% off</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"color":{"text":"#ffffff"}},"textColor":"background","fontSize":"2-xl"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-2-xl-font-size" style="color:#ffffff;font-style:normal;font-weight:600">Footwear</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"foreground","style":{"color":{"background":"#ffffff"},"border":{"radius":"8px","color":"#e5e5e5","width":"1px","style":"solid"},"spacing":{"padding":{"left":"var:preset|spacing|5","right":"var:preset|spacing|5"}},"typography":{"fontWeight":"400","fontStyle":"normal","fontSize":"14px"},"shadow":" 0px 1px 2px 0px #0000000d"}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-foreground-color has-text-color has-background has-border-color has-custom-font-size wp-element-button" style="border-color:#e5e5e5;border-style:solid;border-width:1px;border-radius:8px;background-color:#ffffff;padding-right:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--5);box-shadow: 0px 1px 2px 0px #0000000d;font-size:14px;font-style:normal;font-weight:400">Shop now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></main>
+<!-- /wp:group -->
